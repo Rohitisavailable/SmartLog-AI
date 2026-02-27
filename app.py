940dab7f2f812a5da0c11e3098120c42317e7e78
Rohit Mahajan <tfrohit02@gmail.com>
2026-02-27 13:16:05 +0530

    Implement file upload feature with HTML form and styling

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask , render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file = request.files['file']
+
+    file.save(f'uploads/{file.filename}')
+    return redirect('/')
+
 
 if __name__ == '__main__':
-     app.run(debug=True)
+    app.run(debug=True)
