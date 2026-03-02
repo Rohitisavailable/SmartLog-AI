d42a9d51b4db05d852a047b626fd1f3531a88a55
Rohit Mahajan <tfrohit02@gmail.com>
2026-03-02 11:50:59 +0530

    Refactor file upload handling to include file type validation and error handling for large files

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask , render_template, request, redirect
 from werkzeug.utils import secure_filename
+from werkzeug.exceptions import RequestEntityTooLarge
+
 import os
 
 app = Flask(__name__)
+app.config['UPLOAD_DIRECTORY'] = 'uploads/'
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024 #5 MB
+app.config['ALLOWED_EXTENSIONS'] = ['.log', '.txt', '.csv']
 
 @app.route('/')
 def index():
@@ -10,12 +15,21 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    file = request.files['file']
-    if file:
-        file.save(os.path.join(
-            'uploads/',
-            secure_filename(file.filename)
-        ))
+    try:
+        file = request.files['file']
+        extension = os.path.splitext(file.filename)[1]
+        
+        if file:
+            if file:
+                if extension not in app.config['ALLOWED_EXTENSIONS']:
+                    return 'This File is not allowed'
+            file.save(os.path.join(
+                app.config['UPLOAD_DIRECTORY'],
+                secure_filename(file.filename)
+            ))
+    except RequestEntityTooLarge:
+        return 'File is larger than the 5MB Limit'
+
 
     return redirect('/')
 
